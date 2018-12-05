d74dbe587caf62f87728a2339c4e37a88508eb92
jihoro <44278961+jihoro@users.noreply.github.com>
2018-12-04 19:59:16 -0600

    Update SimpleHuffProcessor.java

--- SimpleHuffProcessor.java
+++ SimpleHuffProcessor.java
@@ -27,13 +27,14 @@ public class SimpleHuffProcessor implements IHuffProcessor {
 	private int[] counts;
 	private HuffTree tree;
 	private int headerFormat;
+	private int treeSize;
 
 	public int compress(InputStream in, OutputStream out, boolean force) throws IOException {
 		//throw new IOException("compress is not implemented");
 		// implement force later!!!!!!!!!
 		BitInputStream bis = new BitInputStream(in);
 		BitOutputStream bos = new BitOutputStream(out);
-		int result = BITS_PER_INT * 2;
+		//int result = BITS_PER_INT * 2;
 		// write the magic number
 		bos.writeBits(BITS_PER_INT, MAGIC_NUMBER);
 		// write the header format
@@ -41,18 +42,25 @@ public class SimpleHuffProcessor implements IHuffProcessor {
 		if(headerFormat == STORE_COUNTS) {
 			for(int k = 0; k < IHuffConstants.ALPH_SIZE; k++) { 
 				bos.writeBits(BITS_PER_INT, counts[k]); 
-				result += BITS_PER_INT;
+				//result += BITS_PER_INT;
 			} 
 		}
 		// if STF
-		// blah blah
+		// need to implement
+		if(headerFormat == STORE_TREE) {
+			treeSize += tree.getSize() * (BITS_PER_WORD + 2); 
+			bos.writeBits(BITS_PER_INT, treeSize);
+			//result += BITS_PER_INT;
+			preorder(tree.getRoot(), bos);
+			//result += bitCount(tree.getRoot());
+		}
 		// writing the actual compressed data
 		int bit = bis.readBits(BITS_PER_WORD);
 		while(bit != -1) {
 			String code = tree.getCode(bit);
 			for(int i = 0; i < code.length(); i++) {
 				bos.writeBits(1, code.charAt(i));
-				result++;
+				//result++;
 			}
 			bit = bis.readBits(BITS_PER_WORD);
 		}
@@ -60,11 +68,40 @@ public class SimpleHuffProcessor implements IHuffProcessor {
 		String eofCode = tree.getCode(PSEUDO_EOF);
 		for(int i = 0; i < eofCode.length(); i++) {
 			bos.writeBits(1, eofCode.charAt(i));
-			result++;
+			//result++;
 		}
-		return result;
+		bis.close();
+		bos.close();
+		return getNumBitOutput(in);
 	}
 
+	private void preorder(TreeNode node, BitOutputStream bos) {
+		if(node != null) {
+			if(!node.isLeaf()) {
+				bos.writeBits(1, 0);
+			}
+			else {
+				bos.writeBits(1, 1);
+				bos.writeBits(BITS_PER_WORD + 1, node.getValue());
+			}
+			preorder(node.getLeft(), bos);
+			preorder(node.getRight(), bos);
+		}
+	}
+
+	private int bitCount(TreeNode node) {
+		if(node != null) {
+			if(!node.isLeaf()) {
+				return 1 + bitCount(node.getLeft()) + bitCount(node.getRight());
+			}
+			else {
+				return BITS_PER_WORD + 2;
+			}
+		}
+		return 0;
+	}
+	
+	
 	public int preprocessCompress(InputStream in, int headerFormat) throws IOException {
 		//showString("Not working yet");
 		//myViewer.update("Still not working");
@@ -87,9 +124,11 @@ public class SimpleHuffProcessor implements IHuffProcessor {
 		}
 		hq.enqueue(new TreeNode(PSEUDO_EOF, 1));
 		tree = new HuffTree(hq.makeTree());
-		System.out.println(getNumBitOutPut(in));
-		System.out.println(result - getNumBitOutPut(in));
-		return result - getNumBitOutPut(in);
+		treeSize = hq.getNumInternalNode();
+		//System.out.println(getNumBitOutPut(in));
+		//System.out.println(result - getNumBitOutPut(in));
+		bis.close();
+		return result - getNumBitOutput(in);
 	}
 
 	/**
@@ -98,15 +137,19 @@ public class SimpleHuffProcessor implements IHuffProcessor {
 	 * @return
 	 * @throws IOException
 	 */
-	private int getNumBitOutPut(InputStream in) throws IOException {
+	private int getNumBitOutput(InputStream in) throws IOException {
 		in.reset();
 		BitInputStream bis = new BitInputStream(in);
 		int result = BITS_PER_INT * 2;
 		// if scf
-		result += BITS_PER_INT * ALPH_SIZE;
+		if(headerFormat == STORE_COUNTS)
+			result += BITS_PER_INT * ALPH_SIZE;
 
 		// if STF
 		// haven't implemented yet
+		else {
+			result += (BITS_PER_INT + bitCount(tree.getRoot()));
+		}
 		// writing the actual compressed data
 		int bit = bis.readBits(BITS_PER_WORD);
 		while(bit != -1) {
@@ -125,22 +168,131 @@ public class SimpleHuffProcessor implements IHuffProcessor {
 	}
 
 	public int uncompress(InputStream in, OutputStream out) throws IOException {
-		throw new IOException("uncompress not implemented");
-		/*BitInputStream bis = new BitInputStream(in);
+		//throw new IOException("uncompress not implemented");
+		BitInputStream bis = new BitInputStream(in);
+		BitOutputStream bos = new BitOutputStream(out);
 		// read magic number
 		int magic = bis.readBits(BITS_PER_INT); 
 		if (magic != MAGIC_NUMBER) {
-		    myViewer.showError("Error reading compressed file. \n" +
-		            "File did not start with the huff magic number.");
-		    return -1;
+			myViewer.showError("Error reading compressed file. \n" +
+					"File did not start with the huff magic number.");
+			return -1;
 		}
+		int store = bis.readBits(BITS_PER_INT);
 		// recreate counts
-		for(int k=0; k < IHuffConstants.ALPH_SIZE; k++) { 
-    		int bits = in.readBits(BITS_PER_INT); 
-    		myCounts[k] = bits; 
-		}	
-		 */
-		//return 0;
+		if(store == STORE_COUNTS) {
+			counts = new int[ALPH_SIZE];
+			// recreate counts
+			for(int k = 0; k < IHuffConstants.ALPH_SIZE; k++) { 
+				int bits = bis.readBits(BITS_PER_INT); 
+				counts[k] = bits; 
+			}
+			// make tree again from counts
+			// can make this a helper method in hufftree or huffqueue
+			HuffQueue<TreeNode> hq = new HuffQueue<>();
+			for(int i = 0; i < counts.length; i++) {
+				if(counts[i] != 0) {				
+					hq.enqueue(new TreeNode(i, counts[i]));
+				}
+			}
+			hq.enqueue(new TreeNode(PSEUDO_EOF, 1));
+			tree = new HuffTree(hq.makeTree());
+		}
+		else {
+			//maybe can improve
+			int size = bis.readBits(BITS_PER_INT);
+			TreeNode node = null;
+			while(size > 0) {
+				int bits = bis.readBits(1);
+				//System.out.println(bits);
+				size--;
+				if(bits == 0)
+					node = restoreTree(node, -1);
+				else {
+					bits = bis.readBits(BITS_PER_WORD + 1);
+					//System.out.println(bits);
+					node = restoreTree(node, bits);
+					size -= (BITS_PER_WORD + 1);
+				}
+				//System.out.println("size " + size);
+			}
+			tree = new HuffTree(node);
+		}
+		return decode(bis, bos);
+	}
+
+	private TreeNode restoreTree(TreeNode node, int value) throws IOException
+	{
+		if(node == null) {
+			return new TreeNode(value, 0);
+		}
+		if(node.getValue() > 0)
+			return node;
+		else {
+			if(!restoreTree(node.getLeft(), value).isLeaf()) {
+					node.setLeft(restoreTree(node.getLeft(), value));
+			}
+			else {
+				if(!restoreTree(node.getRight(), value).isLeaf())
+					node.setRight(restoreTree(node.getRight(), value));
+			}
+			return node;
+		}
+	}
+	
+	
+	private int decode(BitInputStream bis, BitOutputStream bos) throws IOException {
+		boolean done = false;
+		// get ready to walk tree, start at root
+		TreeNode current = tree.getRoot();
+		int result = 0;
+		while(!done) {
+			int bit = bis.readBits(1);
+			// didn't find eof
+			if(bit == -1)
+				throw new IOException("Error reading compressed file. \n" +
+						"unexpected end of input. No PSEUDO_EOF value.");
+			else {
+				System.out.println(current);
+				// move left in tree if bit is 0
+				if(bit == 0)
+					current = current.getLeft();
+				// else move right 
+				else
+					current = current.getRight();
+				// if current is a leaf node
+				if(current.isLeaf()) {
+					// if value is eof value, we are done
+					if(current.getValue() == PSEUDO_EOF)
+						done = true;
+				// else, write out value in leaf and go back to root of tree
+					else {
+						bos.writeBits(BITS_PER_WORD, current.getValue());
+						result += BITS_PER_WORD;
+						current = tree.getRoot();
+					}
+				}
+			}
+		}
+		bis.close();
+		bos.close();
+		return result;
+		/*get ready to walk tree, start at root
+    	boolean done = false;
+    	while(!done) 
+        int bit = bitsIn.readBits(1);
+        if(bit == -1)
+            throw new IOException("Error reading compressed file. \n" +
+                "unexpected end of input. No PSEUDO_EOF value.");
+        else 
+            move left or right in tree based on value of bit
+            (move left if bit is 0, move right if bit is 1)
+            if(reached a leaf node) {
+                if(val is the pseudo end of file value)
+                    done = true;
+                else 
+                    write out value in leaf to output
+                    get back to root of tree*/
 	}
 
 	private void showString(String s){
